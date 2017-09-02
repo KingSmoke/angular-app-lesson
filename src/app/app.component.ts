abdcaa9b1f938d8ee5ba3eec84219b11b1076d89
King Smoke <gochrismail@gmail.com>
2017-09-02 01:24:43 -0400

    Add animations: trigger,state,style,transition,animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,49 @@
 import { Component } from '@angular/core';
 import { MyDataService } from './my-data.service';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations'
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//end selector
   template: `
-
-  <p>{{ someProperty }}</p>
-    
-    `,
-
+    <p [@myAwesomeAnimation]='state' (click)="animateMe()">I will animate</p>
+    `,//end template
   styles: [`
-    .red-title {
-      color: red;
+    p {
+      width:200px;
+      background:lightgray;
+      margin:100px auto;
+      text-align:center;
+      padding:200px;
+      font-size:1.5em;
     }
-  `]
-})
+  `],//end styles
+// trigger accepts state args about direction of change, ie. small to large
+// state accepts style args about CSS
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),//end state
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),// end state
+      transition('small <=> large', animate('300ms ease-in')),//end transition
+    ]),// end trigger
+  ]//end animations
+})//end @component
+
 export class AppComponent {
 
+  state: string = 'small'
+
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
+
+  //constructor from a lesson on services
   constructor(private dataService:MyDataService) {
 
   }
-
-  someProperty:string = '';
-
-  ngOnInit() {
-    console.log(this.dataService.cars);
-
-    this.someProperty = this.dataService.myDataMethod();
-  }
+ 
 
 }
